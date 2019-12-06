a3ef22c5d52875dfb3195b7a1f6db3fdff928b66
Elshaer-X <57920042+Elshaer-X@users.noreply.github.com>
2019-12-06 07:54:18 +0200

    Add files via upload

---
+++ PxScrapper.py
@@ -0,0 +1,73 @@
+import requests
+
+B = str("""
+  ______ _     _                     
+ |  ____| |   | |                    
+ | |__  | |___| |__   __ _  ___ _ __ 
+ |  __| | / __| '_ \ / _` |/ _ \ '__|
+ | |____| \__ \ | | | (_| |  __/ |   
+ |______|_|___/_| |_|\__,_|\___|_|   
+ 
+ TOOL       : [PROXY-SCRAPPER]
+ CREATED BY : MAHMOUD ELSHAER
+ FB         : fb.com/elsha3r.ME                                 
+ 
+ GETTING PROXIES ...                                    
+ """)
+print(B)
+
+def WebsitesList():
+    with open("Px-webs.txt","r") as pxWebs:
+        pxWebs = pxWebs.read().split("\n")
+    return pxWebs
+
+def GetRawData(WebLink):
+    R = requests.get(WebLink)
+    webContent = R.content
+    return webContent
+    
+def WriteProxy(proxy):
+    with open("Results.txt","a") as p:
+        p.write(proxy+"\n")
+def AnalyseRawData(RawData):
+    RawData = str(RawData)
+    toBeReplaced = list("<>!@#$%^&*()+=\ /,|;:'\"-")
+    for Sign in toBeReplaced:
+        RawData = RawData.replace(Sign," ")
+    SplitedRawData = RawData.split(" ")
+    searchForPort = False
+    for Str in SplitedRawData:
+        isProxy = True
+        proxy , port = "" , ""
+        #print(Str)
+        if searchForPort:
+            if Str.isdigit():
+                port = str(Str)
+                Px += port
+                searchForPort = False
+                WriteProxy(Px)
+        elif (Str+"x")[0].isdigit() and Str.count(".") == 3:
+            Px = ""
+            StrL = Str.split(".")
+            for Num in StrL:
+                if Num.isdigit():
+                    pass
+                else:
+                    isProxy = False
+                    break
+            if isProxy == True:
+                proxy = str(Str)
+                searchForPort = True
+                Px += proxy+":"
+            
+def Run():
+    Websites = WebsitesList()
+    for website in Websites:
+        try:
+            RawData = GetRawData(website)
+            AnalyseRawData(RawData)
+        except:
+            pass
+                    
+                    
+Run()
